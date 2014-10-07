e32fba201342335ac1c78929191e9c9b2177eb29
Jeremy Stashewsky <jeremy@goinstant.com>
2014-10-07 11:19:54 -0700

    Minimize dev dependencies

--- test.js
+++ test.js
@@ -15,7 +15,7 @@
  *   Neither the name of Salesforce.com, nor the names of its contributors may
  *   be used to endorse or promote products derived from this software without
  *   specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,7 +36,7 @@ var secureFilters;
 var _;
 
 if (typeof module !== 'undefined' && module.exports) {
-  assert = require('gi-assert');
+  assert = require('assert');
   secureFilters = require('./index');
   _ = require('lodash');
 } else {
